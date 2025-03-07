5c452ad425e4566287b8464b2ac94c351a22ccd3
Ameen Hyder V <150655421+ameenhyder-v@users.noreply.github.com>
2025-03-07 14:10:03 +0530

    Time: 38 ms (89.37%), Space: 54.4 MB (6.1%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.js
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.js
@@ -3,25 +3,27 @@
  * @return {string}
  */
 var sortSentence = function(s) {
-    let newS = s.split(" ")
+    // let newS = s.split(" ")
 
-    let a = [];
-    for(let i = 0; i < newS.length; i++){
-        a.push(parseInt(newS[i][newS[i].length-1]))
-    }
+    // let a = [];
+    // for(let i = 0; i < newS.length; i++){
+    //     a.push(parseInt(newS[i][newS[i].length-1]))
+    // }
   
-    let b = [];
-    for(let i = 0; i < newS.length; i++) {
-        b[a[i] - 1] = newS[i]
-    }
+    // let b = [];
+    // for(let i = 0; i < newS.length; i++) {
+    //     b[a[i] - 1] = newS[i]
+    // }
 
-    for(let i = 0; i < newS.length; i++) {
-        let n = b[i].split("");
-        n.pop();
+    // for(let i = 0; i < newS.length; i++) {
+    //     let n = b[i].split("");
+    //     n.pop();
         
-        b[i] = n.join("")
-    }
-    // console.log(b)
-    return b.join(" ")
-    
+    //     b[i] = n.join("")
+    // }
+    // return b.join(" ")
+
+  return s.split(" ").sort((a,b) => a[a.length-1] - b[b.length - 1])
+        .map((word) => word.slice(0, word.length - 1)).join(" ");
+ 
 };
